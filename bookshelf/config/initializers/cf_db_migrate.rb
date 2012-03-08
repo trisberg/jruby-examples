74252ee9fe05815296d3ec696cf586fed36e891e
Thomas Risberg <trisberg@vmware.com>
2012-03-07 19:13:24 -0500

    cleaned up the db:migrate initializer

--- bookshelf/config/initializers/cf_db_migrate.rb
+++ bookshelf/config/initializers/cf_db_migrate.rb
@@ -1,9 +1,7 @@
-# Be sure to restart your server when you modify this file.
-
 # Run the equivalent of rake db:migrate on startup
 if ENV["VCAP_SERVICES"] != nil
-  config_file = Rails.root.join('db','migrate')
-  if config_file.directory? 
-    ActiveRecord::Migrator.migrate(config_file)
+  db_migrate = Rails.root.join('db','migrate')
+  if db_migrate.directory? 
+    ActiveRecord::Migrator.migrate(db_migrate)
   end
 end
